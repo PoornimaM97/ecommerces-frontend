c47e7bb8598f32cd964f8559e88edfd35b6bb9c2
Poornima.M <poornimamahadev01@gmail.com>
2024-11-12 15:58:44 +0530

    products update

--- src/services/ProductsServices.jsx
+++ src/services/ProductsServices.jsx
@@ -2,4 +2,8 @@ import axios from "axios";
 
 const REST_API_BASE_PRODUCTS_URL = "http://localhost:9191/api/products";
 
+export const addProducts = (productsData) => axios.post(REST_API_BASE_PRODUCTS_URL);
+
 export const listProducts = () => axios.get(REST_API_BASE_PRODUCTS_URL);
+
+export const listProductsById = (id) => axios.get(REST_API_BASE_PRODUCTS_URL + `${id}`)
