adc2964dc0503bf9626df20a7a6b2e1308738ea2
Poornima.M <poornimamahadev01@gmail.com>
2024-11-15 16:31:29 +0530

    users workings

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Navbars from './components/Navbars'; // Adjust the path as needed
 import ProductsListing from './components/ProductsListing';
 import Login from './pages/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';;
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';import Profile from './pages/Profile';
+;
 
 
 // import Home from './pages/Home';
@@ -30,6 +31,7 @@ function App() {
         <Routes>
         <Route path="/login" element={<Login/>} /> 
         <Route path="/products" element={<ProductsListing />} />
+        <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </Router>
